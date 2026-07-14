1a632d5da6f4c206e7340d9026b42977173b5485
J0LaD <joaalmeida215@gmail.com>
2026-07-14 11:46:56 +0200

    feat(app): infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::io;
 
-fn main()
+fn get_input()
 {
     let mut user_input = String::new();
 
-    io::stdin().read_line(&mut user_input).expect("func_error");
-    print!("{}", user_input);
+    loop {
+        user_input.clear();
+        io::stdin().read_line(&mut user_input).expect("func_error");
+        print!("{}", user_input);
+    }
+}
+
+fn main()
+{
+    get_input();
 }
