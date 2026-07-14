8f3cd5cc1e93059346aed8edf3057e132e9c08a1
J0LaD <joaalmeida215@gmail.com>
2026-07-14 12:51:10 +0200

    feat(app): add function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::io;
 
+fn add_to_list(list: &mut Vec<String>)
+{
+    let mut data: String = String::new();
+
+    io::stdin().read_line(&mut data).expect("func_error");
+    list.push(data);
+}
+
 fn get_input()
 {
     let mut list: Vec<String> = Vec::new();
-    
+
     loop {
-        let mut user_input: String = String::new();
-        io::stdin().read_line(&mut user_input).expect("func_error");
-        if user_input.trim() == "Quit" {
+        let mut command: String = String::new();
+        io::stdin().read_line(&mut command).expect("func_error");
+        if command.trim() == "Quit" {
             break;
         }
-        list.push(user_input);
+        if command.trim() == "add" {
+            add_to_list(&mut list);
+        }
     }
     print!("{:?}", list);
 }
